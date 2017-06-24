d5ae287094f20c06c704ee38dc0437de621f8ce5
zsiguli <markovichzsigmond@gmail.com>
2017-06-24 12:54:34 +0200

    feat(USE): use implemented classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    First first = new First();
+    Second second = new Second();
+    first.sout();
+    second.sout();
   }
 }
