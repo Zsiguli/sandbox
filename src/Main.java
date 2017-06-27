b633c28de98eb0c614a1161c7599b101c3579e26
zsiguli <markovichzsigmond@gmail.com>
2017-06-27 13:35:39 +0200

    to be continued

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,22 @@ public class Main {
     for (Connection conn : test) {
       conn.sout();
     }
+    int cap = 2147483639;
+    int max = -1;
+    List<Integer> integers = new ArrayList<>();
+    for (int i = 0; i < cap; ++i) {
+      integers.add(8);
+      int current = i / cap * 100;
+      float currentD = i / cap * 100;
+      System.out.println("i: " + i);
+      System.out.println("cap: " + cap);
+      System.out.println("current: " + current);
+      System.out.println("currentD: " + currentD);
+      System.out.println("max: " + max);
+      if (current > max) {
+        max = current;
+        System.out.println(current + "%");
+      }
+    }
   }
 }
